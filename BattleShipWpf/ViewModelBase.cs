c9cdd2ccc4025aee38002dbdedd017152c645d73
Сергей <acharne1985@gmail.com>
2022-10-17 01:46:07 +0300

    Create class ViewModelBase

---
+++ BattleShipWpf/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BattleShipWpf
+{
+   internal class ViewModelBase : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected void Set<T>(ref T field, T value, [CallerMemberName] string propName = "")
+      {
+         field = value;
+         PropertyChanged(this, new PropertyChangedEventArgs(propName));
+      }
+
+      protected void Fire(params string[] names)
+      {
+         foreach (var name in names)
+         {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+         }
+      }
+   }
+}
